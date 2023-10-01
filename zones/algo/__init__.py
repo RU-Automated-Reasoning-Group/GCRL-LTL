f047568086e213e310b14b0dbd3c65c0e35c2f61
Wenjie Qiu <echo-inception@live.com>
2023-10-01 18:08:31 -0400

    make your life easier

--- zones/algo/__init__.py
+++ zones/algo/__init__.py
@@ -1,3 +1,2 @@
 from algo.scc import path_finding
 from algo.reaching import reaching
-from algo.reaching_q import reaching_q
--- zones/train_gc_policy.py
+++ zones/train_gc_policy.py
@@ -5,6 +5,7 @@ import torch
 import torch.nn as nn
 import gym
 import numpy as np
+from stable_baselines3 import PPO
 from stable_baselines3.common.vec_env import SubprocVecEnv
 from stable_baselines3.common.env_util import make_vec_env
 from stable_baselines3.common.logger import configure
@@ -12,7 +13,6 @@ from stable_baselines3.common.callbacks import EvalCallback
 
 from envs import ZonesEnv, ZoneRandomGoalEnv
 from envs.utils import get_named_goal_vector
-from gc_ppo import GCPPO
 
 
 def main(args):
@@ -22,7 +22,6 @@ def main(args):
     total_timesteps = args.total_timesteps
     num_cpus = args.num_cpus
     seed = args.seed
-    algo = args.algo
     exp_name = args.exp_name
     execution_mode = args.execution_mode
 
@@ -35,48 +34,46 @@ def main(args):
         device=device,
     )
 
-    if algo == 'ppo':
-        env = make_vec_env(env_fn, n_envs=num_cpus, seed=seed, vec_env_cls=SubprocVecEnv)
-        model = GCPPO(
-            policy='MlpPolicy',
-            policy_kwargs=dict(activation_fn=nn.ReLU, net_arch=[512, 1024, 256]),
-            env=env,
-            verbose=1,
-            learning_rate=0.0003,
-            gamma=0.998,
-            n_epochs=10,
-            n_steps=int(50000/num_cpus),
-            batch_size=1000,
-            ent_coef=0.003,
-            device=device,
-        )
+    env = make_vec_env(env_fn, n_envs=num_cpus, seed=seed, vec_env_cls=SubprocVecEnv)
+    model = PPO(
+        policy='MlpPolicy',
+        policy_kwargs=dict(activation_fn=nn.ReLU, net_arch=[512, 1024, 256]),
+        env=env,
+        verbose=1,
+        learning_rate=0.0003,
+        gamma=0.998,
+        n_epochs=10,
+        n_steps=int(50000/num_cpus),
+        batch_size=1000,
+        ent_coef=0.003,
+        device=device,
+    )
 
-        log_path = "logs/ppo/{}/".format(exp_name)
-        new_logger = configure(log_path, ["stdout", "csv"])
-        model.set_logger(new_logger)
+    log_path = "logs/ppo/{}/".format(exp_name)
+    new_logger = configure(log_path, ["stdout", "csv"])
+    model.set_logger(new_logger)
 
-        eval_log_path = "logs/ppo_eval/{}/".format(exp_name)
-        eval_env_fn = lambda: ZoneRandomGoalEnv(
-            env=gym.make('Zones-8-v0', timeout=1000), 
-            primitives_path='models/primitives', 
-            goals_representation=get_named_goal_vector(),
-            use_primitves=True if execution_mode == 'primitives' else False,
-            rewards=[-0.001, 1],
-            device=device,
-        )
-        eval_env = make_vec_env(eval_env_fn)
-        eval_callback = EvalCallback(
-            eval_env=eval_env, 
-            best_model_save_path=eval_log_path,
-            log_path=eval_log_path, 
-            eval_freq=100000/num_cpus,
-            n_eval_episodes=40,
-            deterministic=True,
-        )
+    eval_log_path = "logs/ppo_eval/{}/".format(exp_name)
+    eval_env_fn = lambda: ZoneRandomGoalEnv(
+        env=gym.make('Zones-8-v0', timeout=1000), 
+        primitives_path='models/primitives', 
+        goals_representation=get_named_goal_vector(),
+        use_primitves=True if execution_mode == 'primitives' else False,
+        rewards=[-0.001, 1],
+        device=device,
+    )
+    eval_env = make_vec_env(eval_env_fn)
+    eval_callback = EvalCallback(
+        eval_env=eval_env, 
+        best_model_save_path=eval_log_path,
+        log_path=eval_log_path, 
+        eval_freq=100000/num_cpus,
+        n_eval_episodes=40,
+        deterministic=True,
+    )
     
     model.learn(total_timesteps=total_timesteps, callback=eval_callback)
-    model.save(path='{}_{}_model.zip'.format(algo, seed))
-    model.save_q_net(path='{}_{}_q_net.zip'.format(algo, seed))
+    model.save(path='{}_{}_model.zip'.format(exp_name, seed))
 
 
 if __name__ == '__main__':
@@ -85,7 +82,6 @@ if __name__ == '__main__':
     parser.add_argument('--device', type=str, default='cpu')
     parser.add_argument('--timeout', type=int, default=1000)
     parser.add_argument('--total_timesteps', type=int, default=1.0e7)
-    parser.add_argument('--algo', type=str, default='ppo', choices=('ppo'))
     parser.add_argument('--num_cpus', type=int, default=4)
     parser.add_argument('--seed', type=int, default=123)
     parser.add_argument('--exp_name', type=str, default='exp')
